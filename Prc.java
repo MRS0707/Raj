ba6b5b2ff4fbc7a5990db370b6763746b58ea4b3
MRS0707 <90347900+MRS0707@users.noreply.github.com>
2023-10-03 20:53:29 +0530

    Scanner function add

---
+++ Prc.java
@@ -0,0 +1,11 @@
+package localPrctice;
+
+import java.util.*;
+
+public class Prcactiecondition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
