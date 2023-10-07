33e2d8184ca5c18f66fc1a987c9ad1d3c5cfd452
MRS0707 <90347900+MRS0707@users.noreply.github.com>
2023-10-07 19:19:05 +0530

    Hello Raj

--- Prc.java
+++ Prc.java
@@ -2,9 +2,10 @@ package localPrctice;
 
 import java.util.*;
 
-public class Prcactiecondition {
+public class Prc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hello Raj");
 
     }
 
